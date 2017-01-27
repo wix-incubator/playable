8055cc8b8cb962039885844ba317802d6d51594d
Alexey Kupriyanenko <alexeyk@wix.com>
2017-01-27 19:18:53 +0200

    Add hot reload support

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -1,5 +1,6 @@
 import playIcon from '../controls/play-icon.html';
 import pauseIcon from '../controls/pause-icon.html';
+import styles from 'app.css';
 
 
 function getUI(config) {
@@ -13,23 +14,22 @@ function getUI(config) {
 
   function createWrapper() {
     const wrapper = document.createElement('div');
-    wrapper.setAttribute('class', 'video-wrapper');
-
+    wrapper.setAttribute('class', styles['video-wrapper']);
     return wrapper;
   }
 
   function createControls() {
     const wrapper = document.createElement('div');
-    wrapper.setAttribute('class', 'controls-wrapper');
+    wrapper.setAttribute('class', styles['controls-wrapper']);
 
     const background = document.createElement('div');
-    background.setAttribute('class', 'controls-background');
+    background.setAttribute('class', styles['controls-background']);
 
     const innerWrapper = document.createElement('div');
-    innerWrapper.setAttribute('class', 'controls');
+    innerWrapper.setAttribute('class', styles['controls']);
 
     const controlRow = document.createElement('div');
-    controlRow.setAttribute('class', 'controls-row');
+    controlRow.setAttribute('class', styles['controls-row']);
 
     controlRow.appendChild(createPlayControl());
 
@@ -43,15 +43,15 @@ function getUI(config) {
 
   function createPlayControl() {
     const _playButton = document.createElement('div');
-    _playButton.setAttribute('class', 'play');
+    _playButton.setAttribute('class', styles['play']);
 
     const _playIcon = document.createElement('div');
-    _playIcon.setAttribute('class', 'play-icon icon');
+    _playIcon.setAttribute('class', `${styles['play-icon']} ${styles['icon']}`);
     _playIcon.innerHTML = playIcon;
     _playIcon.addEventListener('click', playVideo);
 
     const _pauseIcon = document.createElement('div');
-    _pauseIcon.setAttribute('class', 'pause-icon icon');
+    _pauseIcon.setAttribute('class', `${styles['pause-icon']} ${styles['icon']}`);
     _pauseIcon.innerHTML = pauseIcon;
     _pauseIcon.addEventListener('click', pauseVideo);
 
@@ -63,12 +63,12 @@ function getUI(config) {
 
   function playVideo() {
     onPlayClick();
-    _wrapper.classList.toggle('video-playing');
+    _wrapper.classList.toggle(styles['video-playing']);
   }
 
   function pauseVideo() {
     onPauseClick();
-    _wrapper.classList.toggle('video-playing');
+    _wrapper.classList.toggle(styles['video-playing']);
   }
 
 }
---
+++ test/mock/fake-server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { render } from './tmpl';
+
+export function start(port = 3000) {
+  const app = express();
+
+  app.use('/', (req, res) => {
+    res.send(render('./test/mock/index.ejs'));
+  });
+
+  return app.listen(port, () => {
+    console.log(`Fake server is running on port ${port}`);
+  });
+}
