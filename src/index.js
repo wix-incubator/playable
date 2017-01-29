add1b38d60e091c6911aa226a96c87414b4d7cf1
Alexey Kupriyanenko <alexeyk@wix.com>
2017-01-29 02:24:36 +0200

    Fix test page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import VideoPlayer from './player';
 
+
+// TODO: Temp solution, before wix-node-build will support library settings
+window.VideoPlayer = VideoPlayer;
+
 module.exports = VideoPlayer;
