4d3ea62b700ee1f6eeee190217350f323f49ca3c
Alexey Kupriyanenko <alexeyk@wix.com>
2017-01-30 18:17:14 +0200

    build project as UMD

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import VideoPlayer from './player';
 
 
-// TODO: Temp solution, before wix-node-build will support library settings
-window.VideoPlayer = VideoPlayer;
-
 module.exports = VideoPlayer;
