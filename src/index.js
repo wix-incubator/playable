defefb44d00be569868984641f80347661a640ea
Oleksii Makodzeba <oleksiim@wix.com>
2017-02-16 15:35:49 +0200

    Add export for constants

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import VideoPlayer from './player';
+import UI_EVENTS from './constants/events/ui';
+import VIDEO_EVENTS from './constants/events/video';
 
-module.exports = VideoPlayer;
+
+module.exports = {
+  UI_EVENTS,
+  VIDEO_EVENTS,
+  Player: VideoPlayer
+};
