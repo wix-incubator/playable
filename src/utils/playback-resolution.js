ef0663fb706615a7c8cfe453f54fae29e28ec11e
Oleksii Makodzeba <oleksiim@wix.com>
2017-08-10 19:13:34 +0300

    Changed logic for getting proper url from set

--- src/utils/playback-resolution.js
+++ src/utils/playback-resolution.js
@@ -1,21 +1,26 @@
 export function resolvePlayableStreams(mediaStreams, playableStreamCreators, eventEmitter) {
-    const playableStreams = [];
-    const groupedStreams = groupStreamsByMediaType(mediaStreams);
-    Object.keys(groupedStreams).forEach(mediaType => {
-        const mediaStreams = groupedStreams[mediaType];
-        for (let i = 0; i < playableStreamCreators.length; i += 1) {
-            const playableStreamCreator = playableStreamCreators[i];
-            if (playableStreamCreator.canPlay(mediaType)) {
-                playableStreams.unshift(new playableStreamCreator(mediaStreams, eventEmitter));
-                break;
-            }
-        }
-    });
-    playableStreams.sort(
-      (firstStream, secondStream) =>
-        secondStream.getMediaStreamDeliveryType() - firstStream.getMediaStreamDeliveryType()
-    );
-    return playableStreams;
+  const playableStreams = [];
+
+  const groupedStreams = groupStreamsByMediaType(mediaStreams);
+  const groupedStreamKeys = Object.keys(groupedStreams);
+
+  playableStreamCreators.forEach(playableStreamCreator => {
+    for (let i = 0; i < groupedStreamKeys.length; i += 1) {
+      const mediaType = groupedStreamKeys[i];
+      const mediaStreams = groupedStreams[mediaType];
+      if (playableStreamCreator.canPlay(mediaType)) {
+        playableStreams.push(new playableStreamCreator(mediaStreams, eventEmitter));
+        break;
+      }
+    }
+  });
+
+  playableStreams.sort(
+    (firstStream, secondStream) =>
+      secondStream.getMediaStreamDeliveryType() - firstStream.getMediaStreamDeliveryType()
+  );
+
+  return playableStreams;
 }
 
 function groupStreamsByMediaType(mediaStreams) {
