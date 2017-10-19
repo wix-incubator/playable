fd995157da282683cd6909cee858515bc390790d
Stanislav Prokopiv <stanislavp@wix.com>
2017-10-19 12:27:49 +0300

    fix demo cards timing

--- video-player-cards/src/demo/demo-cards.js
+++ video-player-cards/src/demo/demo-cards.js
@@ -37,13 +37,13 @@ export function getDemoCards() {
   const card1 = {
     contentNode: createCard('Test card', 'Test title lOrem ipsuim pisuinipsum.', 'https://scontent-frx5-1.cdninstagram.com/t51.2885-15/e35/16123864_171964476622622_1055696102294028288_n.jpg'),
     from: 0,
-    to: 15
+    to: 30000
   };
 
   const card11 = {
     contentNode: createCard('Test card 2!!!', 'It\'s another card, boy!'),
-    from: 5,
-    to: 15
+    from: 5000,
+    to: 30000
   };
 
   const card1111Node = document.createElement('div');
@@ -51,8 +51,8 @@ export function getDemoCards() {
 
   const card1111 = {
     contentNode: createCard('Test card3', 'Test title lOrem ipsuim pisuinipsum.', 'https://scontent-frx5-1.cdninstagram.com/t51.2885-15/e35/20590144_1012019698938562_2521454400447184896_n.jpg'),
-    from: 8,
-    to: 23
+    from: 8000,
+    to: 30000
   };
 
   const card4Node = document.createElement('div');
@@ -60,8 +60,8 @@ export function getDemoCards() {
 
   const card4 = {
     contentNode: card4Node,
-    from: 15,
-    to: 25
+    from: 15000,
+    to: 30000
   };
 
   const card5Node = document.createElement('div');
@@ -69,8 +69,8 @@ export function getDemoCards() {
 
   const card5 = {
     contentNode: card5Node,
-    from: 20,
-    to: 30
+    from: 20000,
+    to: 30000
   };
 
   return [card1, card11, card1111, card4, card5];
