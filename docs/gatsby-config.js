dceefd3784f166d426d40e5029354b60adea5b21
Bohdan Lyzanets <bohdanl@wix.com>
2017-11-21 14:50:45 +0200

    initial doc

---
+++ docs/gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-remark',
+    'gatsby-plugin-catch-links',
+  ],
+};
