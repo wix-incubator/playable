c29c5b6219c2700a96661acf4e9ea87c2d167f9c
BrooklynKing <oleksiim@wix.com>
2018-01-11 15:15:18 +0200

    Add workaround for possible empty type

--- src/default-modules/playback-engine/adapters/hls.ts
+++ src/default-modules/playback-engine/adapters/hls.ts
@@ -82,8 +82,8 @@ export default class HlsAdapter implements IPlaybackAdapter {
   get isSeekAvailable(): boolean {
     if (this.isDynamicContent) {
       const { details } = this.hls.levels[this.hls.firstLevel];
-      const type = details.type.trim();
-      return type === 'EVENT';
+      const type = details.type || '';
+      return type.trim() === 'EVENT';
     }
 
     return true;
