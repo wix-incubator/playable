0f5dc8cb11bf783dcc4f5a96601ac1ce9ff87181
Bohdan Lyzanets <bohdanl@wix.com>
2018-04-17 18:14:10 +0300

    ci: add saucelabs launchers to karma config

--- dev-env/karma-with-adapters.conf.js
+++ dev-env/karma-with-adapters.conf.js
@@ -1,78 +1,13 @@
-// Karma configuration
+const configBase = require('./karma.conf.base');
 
 /* ignore coverage */
-module.exports = function (config) {
-    config.set({
+module.exports = function(config) {
+  configBase(config, {
+    testName: 'Playable with adapters'
+  });
 
-        // base path that will be used to resolve all patterns (eg. files, exclude)
-        basePath: '',
-
-
-        // frameworks to use
-        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-        frameworks: ['mocha'],
-
-
-        // list of files / patterns to load in the browser
-        files: [
-            '../dist/statics/playable-test-with-adapters.bundle.js'
-        ],
-
-
-        // list of files to exclude
-        exclude: [
-        ],
-
-
-        // preprocess matching files before serving them to the browser
-        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {
-        },
-
-
-        plugins: [
-          require('karma-mocha'),
-          require('karma-chrome-launcher')
-        ],
-
-        // web server port
-        port: 9876,
-
-
-        // enable / disable colors in the output (reporters and logs)
-        colors: true,
-
-
-        // level of logging
-        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_INFO,
-
-
-        // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: true,
-
-
-        // start these browsers
-        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['ChromeHeadlessNoSandbox'],
-
-        customLaunchers: {
-          // https://docs.travis-ci.com/user/chrome#Sandboxing
-          ChromeHeadlessNoSandbox: {
-            base: 'ChromeHeadless',
-            flags: ['--no-sandbox']
-          }
-        },
-
-        // Continuous Integration mode
-        // if true, Karma captures browsers, runs the tests and exits
-        singleRun: true,
-
-        // Concurrency level
-        // how many browser should be started simultaneous
-        concurrency: Infinity,
-
-        // https://docs.travis-ci.com/user/gui-and-headless-browsers/#Karma-and-Firefox-inactivity-timeouts
-        browserNoActivityTimeout: 30000
-    });
+  config.set({
+    // list of files / patterns to load in the browser
+    files: ['../dist/statics/playable-test-with-adapters.bundle.js'],
+  });
 };
