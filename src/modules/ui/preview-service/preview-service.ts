86620d267c1f235ea642bc72eeb9f7c36497879f
Oleksii Makodzeba <piratexpirate@gmail.com>
2018-08-25 16:09:15 +0300

    feat(preview-service): adding service for setting and getting preview of video frames

---
+++ src/modules/ui/preview-service/preview-service.ts
@@ -0,0 +1,45 @@
+import playerAPI from '../../../core/player-api-decorator';
+
+import { IPlaybackEngine } from '../../playback-engine/types';
+
+import { getAt } from './adapter';
+import {
+  IPreviewService,
+  IFramesData,
+  INormalizedFramesQuality,
+} from './types';
+
+export default class PreviewService implements IPreviewService {
+  static moduleName = 'previewService';
+  static dependencies = ['engine'];
+
+  private _engine: IPlaybackEngine;
+
+  private _framesMap: IFramesData;
+
+  constructor({ engine }: { engine: IPlaybackEngine }) {
+    this._engine = engine;
+  }
+
+  @playerAPI()
+  setFramesMap(map: IFramesData) {
+    this._framesMap = map;
+  }
+
+  getAt(second: number): INormalizedFramesQuality[] {
+    if (!this._framesMap) {
+      return;
+    }
+
+    const duration = this._engine.getDurationTime();
+    if (!duration) {
+      return;
+    }
+
+    return getAt(this._framesMap, second, duration);
+  }
+
+  destroy(): void {
+    this._engine = null;
+  }
+}
