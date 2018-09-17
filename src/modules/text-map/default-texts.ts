5a6bb124714227205b72656baba396479ba7f499
BrooklynKing <oleksiim@wix.com>
2018-09-17 13:03:08 +0300

    fix(download-button): addint aria-label to button and small changes to default texts

--- src/modules/text-map/default-texts.ts
+++ src/modules/text-map/default-texts.ts
@@ -2,11 +2,11 @@ import { TEXT_LABELS } from '../../constants';
 import { ITextMapConfig } from './types';
 
 const map: ITextMapConfig = {
-  [TEXT_LABELS.LOGO_LABEL]: 'Watch On Site',
+  [TEXT_LABELS.LOGO_LABEL]: 'Watch on site',
   [TEXT_LABELS.LOGO_TOOLTIP]: 'Watch On Site',
   [TEXT_LABELS.LIVE_INDICATOR_TEXT]: ({ isEnded }) =>
     !isEnded ? 'Live' : 'Live Ended',
-  [TEXT_LABELS.LIVE_SYNC_LABEL]: 'Sync to Live',
+  [TEXT_LABELS.LIVE_SYNC_LABEL]: 'Sync to live',
   [TEXT_LABELS.LIVE_SYNC_TOOLTIP]: 'Sync to Live',
   [TEXT_LABELS.PAUSE_CONTROL_LABEL]: 'Pause',
   [TEXT_LABELS.PLAY_CONTROL_LABEL]: 'Play',
@@ -19,12 +19,12 @@ const map: ITextMapConfig = {
   [TEXT_LABELS.UNMUTE_CONTROL_TOOLTIP]: 'Unmute',
   [TEXT_LABELS.VOLUME_CONTROL_LABEL]: 'Volume control',
   [TEXT_LABELS.VOLUME_CONTROL_VALUE]: ({ volume }) => `Volume is ${volume}%`,
-  [TEXT_LABELS.ENTER_FULL_SCREEN_LABEL]: 'Enter full screen mode',
-  [TEXT_LABELS.ENTER_FULL_SCREEN_TOOLTIP]: 'Enter full screen mode',
-  [TEXT_LABELS.EXIT_FULL_SCREEN_LABEL]: 'Exit full screen mode',
-  [TEXT_LABELS.EXIT_FULL_SCREEN_TOOLTIP]: 'Exit full screen mode',
-  [TEXT_LABELS.DOWNLOAD_BUTTON_LABEL]: 'Download',
-  [TEXT_LABELS.DOWNLOAD_BUTTON_TOOLTIP]: 'Download',
+  [TEXT_LABELS.ENTER_FULL_SCREEN_LABEL]: 'Enter full screen',
+  [TEXT_LABELS.ENTER_FULL_SCREEN_TOOLTIP]: 'Enter Full Screen',
+  [TEXT_LABELS.EXIT_FULL_SCREEN_LABEL]: 'Exit full screen',
+  [TEXT_LABELS.EXIT_FULL_SCREEN_TOOLTIP]: 'Exit Full Screen',
+  [TEXT_LABELS.DOWNLOAD_BUTTON_LABEL]: 'Download video',
+  [TEXT_LABELS.DOWNLOAD_BUTTON_TOOLTIP]: 'Download Video',
 };
 
 export default map;
