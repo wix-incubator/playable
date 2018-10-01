9a3151f9487da9842490a97ff513c5000b06aed9
BrooklynKing <oleksiim@wix.com>
2018-10-01 12:30:21 +0300

    chore(tests): adding test for SSR that playable not using WebAPI on module init

---
+++ src/e2e/node.js
@@ -0,0 +1,4 @@
+// Test that we not using any WebAPI on module initialization
+const playable = require('../../dist/statics/playable.bundle');
+console.log('Playable bundle succesfully imported!');
+process.exit(0);
