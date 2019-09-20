76db181ba7096f9b74edf18cf5a61b2e743d4930
Dmytro Rudnytskykh <rudnitskih@gmail.com>
2019-09-20 15:16:22 +0300

    More storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,20 +1,24 @@
 import { addDecorator, addParameters, configure } from '@storybook/react';
-import {withA11y} from '@storybook/addon-a11y';
-import {withKnobs} from '@storybook/addon-knobs';
+import { withA11y } from '@storybook/addon-a11y';
+import { withKnobs } from '@storybook/addon-knobs';
 import { create } from '@storybook/theming';
 
 addParameters({
   options: {
     theme: create({
       base: 'light',
-      brandTitle: 'Get Domain',
+      brandTitle: 'Playable',
       brandUrl: 'https://github.com/wix/playable',
       brandImage: null,
     }),
+    panelPosition: 'right',
+    showNav: false,
   },
 });
 
 addDecorator(withA11y);
 addDecorator(withKnobs);
+
+require('./styles.scss');
 
 configure(require.context('../src', true, /\.stories\.tsx$/), module);
