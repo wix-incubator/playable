092ff29bcb5e594f9a0a75732220df20c3228342
Oleksii Makodzeba <piratexpirate@gmail.com>
2019-12-05 14:49:01 +0200

    Revert "chore(storybook): move storybook to html instead of react"
    This reverts commit ab2f157ed19597349788895d1aa39e617a00bc4a.

--- src/stories/types.ts
+++ src/stories/types.ts
@@ -1,9 +1,12 @@
-import { MEDIA_STREAM_TYPES } from '../';
+import { IPlayerInstance, MEDIA_STREAM_TYPES } from '../index';
 import { IPlayerConfig } from '../core/config';
 
-export type StoryProps = IPlayerConfig &
-  Partial<{
-    videoType: MEDIA_STREAM_TYPES;
-    color: string;
-    progressBarMode: string;
-  }>;
+export type StoryProps = IPlayerConfig & {
+  videoType: MEDIA_STREAM_TYPES;
+  color: string;
+  progressBarMode: string;
+};
+
+export interface StoryWindow extends Window {
+  player: IPlayerInstance;
+}
